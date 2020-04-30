b446d08b3ccd90c19e61beb44565e05b5698378a
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-04-30 11:58:06 -0400

    where to begin

---
+++ domPrint.js
@@ -0,0 +1,6 @@
+// fetch
+fetch ('https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&apikey=WoH918XzQjQACHV4jJJlI2oUqLqtJXUz')
+.then(response => response.json())
+.then(genreChoice => {
+
+}
