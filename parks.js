f9db3a81710a5a2dee9b29825db19640421331da
Ashon Woodbury <a.woodbury01@gmail.com>
2020-04-30 13:09:37 -0400

    edited fetch call

--- parks.js
+++ parks.js
@@ -2,11 +2,11 @@ fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${parkInput}=Yes`),
   "$$app_token": yourToken
 
 
-.then{(parks) => (parks.json)}
+// .then{(parks) => (parks.json)}
 
-.then{(parsedParks) => {
-parsedParks.forEach(element => {
-    if (element.park_name === "Potters Field") {
-        console.log(element)
-    }
-)}
+// .then{(parsedParks) => {
+// parsedParks.forEach(element => {
+//     if (element.park_name === "Potters Field") {
+//         console.log(element)
+//     }
+// )}
