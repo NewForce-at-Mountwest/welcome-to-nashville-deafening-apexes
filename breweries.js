4c78bee4d1425220c640da655ca4b3240a0199b8
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-05-01 09:39:16 -0400

    Finsihed some work on functions, no results fixed

--- breweries.js
+++ breweries.js
@@ -10,6 +10,10 @@ function fetchBreweries() {
         // Then - Use the info
         .then((parsedBreweries) => {
             // For each loop to go through the return and only show results for the correct state since other states have Nashvilles as well.
+            //
+            // Try moving the if statements on the outside of the loop only loop if the if statements are correct
+            // Code seems to be looping through all actions a lot and could be the cause of the issue on returning "search not found"
+            // Since if statements are on the inside the code is looping even if conditionals are not met. Do ifs then foreach in ifs
             parsedBreweries.forEach(element => {
                 if (breweryUserInput != "") {
                     if (element.state === "Tennessee" && element.name.toLowerCase().includes(breweryUserInput.toLowerCase())) { //Checking for state of TN and user search value && lower casing everything for the compare
@@ -33,12 +37,12 @@ function checkBreweryInput() {
 // Testing a function to clear the variable
 function clearBreweryVar() {
     breweryUserInput = ""
+    document.querySelector(`#search-results`).innerHTML = ""    //Clearing the inner HTML when the button is pressedd
 }
 
 // Creating a function to combine the other functions in use for the click event
 function breweryClickEvent() {
     clearBreweryVar()
-    document.querySelector(`#search-results`).innerHTML = ""    //Clearing the inner HTML when the button is pressedd
     checkBreweryInput()
     fetchBreweries()
     document.querySelector(`#brewery-search`).value = ""    //Cearing the value from the input field when the users request has been ran
