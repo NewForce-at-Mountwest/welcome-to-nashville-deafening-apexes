37e70a44a55dd10c425db346aa9babf464f5eb7f
Ashon Woodbury <a.woodbury01@gmail.com>
2020-05-05 11:27:24 -0400

    added save button functionality to parks

--- parks.js
+++ parks.js
@@ -1,6 +1,6 @@
 document.querySelector("#park-button").addEventListener("click", function () {
   parkSearchValue = document.querySelector("#park-search").value;
-//   document.querySelector("#park-search").value = "";
+  document.querySelector("#park-search").value = "";
     // Clear past results button
     clearEntries()
 
@@ -14,10 +14,15 @@ document.querySelector("#park-button").addEventListener("click", function () {
         if (element.park_name.toLowerCase().includes
         // Filter for user input to match park name
         (parkSearchValue.toLowerCase()) === true) {
-            document.querySelector("#search-results").innerHTML += parkPrinter(element.park_name, element.mapped_location_address)
+            
+            let i = 0;
+            
+            document.querySelector("#search-results").innerHTML += parkPrinter(element.park_name, element.mapped_location_address);
+
+            stringArray[i] = parkPrinter(element.park_name, element.mapped_location_address)
+
+            document.querySelector("#search-results").innerHTML += `<button class="save-button" id="save-park-${i}">Save</button><br></br>`;
+            i++;
         }})
     });
 })
-
-
-    // 
