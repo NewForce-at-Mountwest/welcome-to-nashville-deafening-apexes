9edd62d1b41fe1c85586a7dbed5307f6496aa528
Ashon Woodbury <a.woodbury01@gmail.com>
2020-05-05 09:49:49 -0400

    Corrected else errors

--- parks.js
+++ parks.js
@@ -1,23 +1,26 @@
 document.querySelector("#park-button").addEventListener("click", function () {
   parkSearchValue = document.querySelector("#park-search").value;
-  document.querySelector("#park-search").value = "";
+//   document.querySelector("#park-search").value = "";
+    // Clear past results button
+    clearEntries()
 
     
     // Fetch parks api
     fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?`)
     .then(r => r.json())
     .then (parks => {
-        // Clear past results button
-        clearEntries()
-        // Parks loop
+        // parks loop
         parks.forEach(element => {
-        if (element.park_name.includes(parkSearchValue) === true) {
+        if (element.park_name.toLowerCase().includes
+        // Filter for user input to match park name
+        (parkSearchValue.toLowerCase()) === true) {
             document.querySelector("#search-results").innerHTML += parkPrinter(element.park_name, element.mapped_location_address)
-        } 
-        // else if (element.park_name.includes(parkSearchValue) !== true){
-        //     document.querySelector("#search-results").innerHTML += `<p>No Results Found</p>`
-        // }
+        }
+
     })
             
         });
     })
+
+
+    // 
